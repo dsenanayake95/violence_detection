6b8cafc2c78283404ac0cbb49a548b558028de3f
Dehaja Senanayake <dehaja.senanayake@gmail.com>
2021-09-02 18:58:58 +0100

    initial front end

--- app.py
+++ app.py
@@ -1,25 +1,76 @@
 import streamlit as st 
+from streamlit_player import st_player
 import requests
 
 
 import numpy as np
 import pandas as pd
 
-st.markdown("""# This is a header
-## This is a sub header
-This is text""")
 
-df = pd.DataFrame({
-          'first column': list(range(1, 11)),
-          'second column': np.arange(10, 101, 10)
-        })
+st.sidebar.markdown(f"""
+    # Navigation menu
+    """)
 
-# this slider allows the user to select a number of lines
-# to display in the dataframe
-# the selected value is returned by st.slider
-line_count = st.slider('Select a line count', 1, 10, 3)
+direction = st.radio('Select a page', ('About the project', 'Meet the team', 'Try the model'))
 
-# and used in order to select the displayed lines
-head_df = df.head(line_count)
+st.write(direction)
 
-head_df
+#########################################
+# Title and introduction to the project #
+######################################### 
+
+if direction == 'About the project':
+    st.markdown("""# Violence Detection
+## Detecting violence in uploaded videos
+
+Check it out yourself below. 
+""")
+
+    if st.button('About the project'):
+        print('button clicked!')
+        st.write('This project has looked at identifing the probability of violent behaviour \
+             in a video, in real time. Currently, the most common way to identify this behavour \
+                 is using "human monitors". The extended exposure to violence in videos \
+                     can cause harm to the mental health of these individuals.')
+    else:
+        st.write('Why is detecting violence important?')
+    
+#########################################
+#           Meet the team               #
+######################################### 
+
+elif direction == 'Meet the team':
+    col = st.columns(3)
+    
+    col[0].header("Gift Opar")
+    col[1].header("Lukas (Tu) Pham")
+    col[2].header("Dehaja Senanayake")
+    
+
+#else: direction == 'Try the model':
+    
+    #title = st.text_input('Youtube URL', 'Insert URL here')
+    
+    #st_player(title)
+    
+#########################################
+#     Insert the URL onto the page      #
+######################################### 
+
+
+
+#########################################
+#           Upload a video              #
+######################################### 
+
+#upload = st.empty()
+#start_button = st.empty()
+#stop_button = st.empty()
+
+#with upload:
+    #f = st.file_uploader('Upload Video file (mpeg/mp4 format)', key = state.upload_key)
+    #if f is not None:
+        #tfile  = tempfile.NamedTemporaryFile(delete = True)
+        #tfile.write(f.read())
+
+
